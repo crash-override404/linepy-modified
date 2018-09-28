3ab79d6e05f4ebafb3930ef130cde422c6994033
{Zero Cool} <ancha.roca002@gmail.com>
2018-09-28 14:56:32 +0800

    Update session.py

--- linepy/session.py
+++ linepy/session.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
-from thrift.transport import THttpClient
+from .transport import THttpClient
 from thrift.protocol import TCompactProtocol
 from akad import AuthService, TalkService, ChannelService, CallService, SquareService, ShopService
 
 class Session:
 
-    def __init__(self, url, headers, path=''):
+    def __init__(self, url, headers, path='', customThrift=False):
         self.host = url + path
         self.headers = headers
+        self.customThrift = customThrift
 
     def Auth(self, isopen=True):
-        self.transport = THttpClient.THttpClient(self.host)
+        self.transport = THttpClient(self.host, customThrift=self.customThrift)
         self.transport.setCustomHeaders(self.headers)
         
         self.protocol = TCompactProtocol.TCompactProtocol(self.transport)
@@ -22,7 +23,7 @@ class Session:
         return self._auth
 
     def Talk(self, isopen=True):
-        self.transport = THttpClient.THttpClient(self.host)
+        self.transport = THttpClient(self.host, customThrift=self.customThrift)
         self.transport.setCustomHeaders(self.headers)
         
         self.protocol = TCompactProtocol.TCompactProtocol(self.transport)
@@ -34,7 +35,7 @@ class Session:
         return self._talk
 
     def Channel(self, isopen=True):
-        self.transport = THttpClient.THttpClient(self.host)
+        self.transport = THttpClient(self.host, customThrift=self.customThrift)
         self.transport.setCustomHeaders(self.headers)
 
         self.protocol = TCompactProtocol.TCompactProtocol(self.transport)
@@ -46,7 +47,7 @@ class Session:
         return self._channel
 
     def Call(self, isopen=True):
-        self.transport = THttpClient.THttpClient(self.host)
+        self.transport = THttpClient(self.host, customThrift=self.customThrift)
         self.transport.setCustomHeaders(self.headers)
 
         self.protocol = TCompactProtocol.TCompactProtocol(self.transport)
@@ -58,7 +59,7 @@ class Session:
         return self._call
 
     def Square(self, isopen=True):
-        self.transport = THttpClient.THttpClient(self.host)
+        self.transport = THttpClient(self.host, customThrift=self.customThrift)
         self.transport.setCustomHeaders(self.headers)
 
         self.protocol = TCompactProtocol.TCompactProtocol(self.transport)
@@ -70,7 +71,7 @@ class Session:
         return self._square
 
     def Shop(self, isopen=True):
-        self.transport = THttpClient.THttpClient(self.host)
+        self.transport = THttpClient(self.host, customThrift=self.customThrift)
         self.transport.setCustomHeaders(self.headers)
 
         self.protocol = TCompactProtocol.TCompactProtocol(self.transport)
