9329f913df392e4934bb71267302db92cd869bac
{Zero Cool} <ancha.roca002@gmail.com>
2018-09-28 15:00:34 +0800

    Add customThrift

--- linepy/client.py
+++ linepy/client.py
@@ -11,15 +11,37 @@ from .shop import Shop
 
 class LINE(Auth, Models, Talk, Square, Call, Timeline, Shop):
 
-    def __init__(self, idOrAuthToken=None, passwd=None, certificate=None, systemName=None, appType=None, appName=None, showQr=False, channelId=None, keepLoggedIn=True):
-        Auth.__init__(self, appType)
+    def __init__(self, idOrAuthToken=None, passwd=None, **kwargs):
+        """
+        :param idOrAuthToken: Login email, token. Default: None
+        :param passwd: Login password. Default: None
+        :param kwargs: See below
+        :Keyword Arguments:
+            - **certificate**: Line certificate after email login. Default: None
+            - **systemName**: System name when first login. Default: None
+            - **appType**: Application type to login. Default: None
+            - **appName**: Application name to login. Default: None
+            - **showQr**: Print out qr code. Default: False
+            - **channelId**: Channel ID to login Timeline. Default: None
+            - **keepLoggedIn**: Keep logged in if succesfull login. Default: True
+            - **customThrift**: Increase speed thrift with custom thrift. Default: False
+        :return:
+        """
+        self.certificate = kwargs.pop('certificate', None)
+        self.systemName = kwargs.pop('systemName', None)
+        self.appType = kwargs.pop('appType', None)
+        self.appName = kwargs.pop('appName', None)
+        self.showQr = kwargs.pop('showQr', False)
+        self.channelId = kwargs.pop('channelId', None)
+        self.keepLoggedIn = kwargs.pop('keepLoggedIn', True)
+        self.customThrift = kwargs.pop('customThrift', False)
+        Auth.__init__(self)
         if not (idOrAuthToken or idOrAuthToken and passwd):
-            self.loginWithQrCode(keepLoggedIn=keepLoggedIn, systemName=systemName, appName=appName, showQr=showQr)
+            self.loginWithQrCode()
         if idOrAuthToken and passwd:
-            self.loginWithCredential(_id=idOrAuthToken, passwd=passwd, certificate=certificate, systemName=systemName, appName=appName, keepLoggedIn=keepLoggedIn)
+            self.loginWithCredential(idOrAuthToken, passwd)
         elif idOrAuthToken and not passwd:
-            self.loginWithAuthToken(authToken=idOrAuthToken, appName=appName)
-        self.channelId = channelId
+            self.loginWithAuthToken(idOrAuthToken)
         self.__initAll()
 
     def __initAll(self):
